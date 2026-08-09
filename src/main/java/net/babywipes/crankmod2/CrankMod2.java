61c22e124376133fc623ed81e34e1294a3b2b748
jpshan <jpshan2004@outlook.com>
2026-08-09 04:15:07 -0400

    added data gen for items

--- src/main/java/net/babywipes/crankmod2/CrankMod2.java
+++ src/main/java/net/babywipes/crankmod2/CrankMod2.java
@@ -1,6 +1,7 @@
 package net.babywipes.crankmod2;
 
 import net.babywipes.crankmod2.creativemodetab.ModCreativeModeTabs;
+import net.babywipes.crankmod2.entity.ModEntityTypes;
 import net.babywipes.crankmod2.item.ModItems;
 import net.babywipes.crankmod2.sounds.ModSounds;
 import net.fabricmc.api.ModInitializer;
@@ -17,6 +18,7 @@ public class CrankMod2 implements ModInitializer {
 		ModCreativeModeTabs.registerModCreativeModeTabs();
 		ModItems.registerModItems();
         ModSounds.initalize();
+		ModEntityTypes.registerModEntityTypes();
 	}
 }
 
---
+++ src/main/java/net/babywipes/crankmod2/entity/ModEntityTypes.java
@@ -0,0 +1,33 @@
+package net.babywipes.crankmod2.entity;
+
+import net.babywipes.crankmod2.CrankMod2;
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
+
+public class ModEntityTypes {
+    public static final EntityType<GorillaEntity> GORILLA = register(
+            "gorilla",
+            EntityType.Builder.<GorillaEntity>of(GorillaEntity::new, MobCategory.MISC)
+                    .sized(0.75f, 1.75f)
+    );
+
+    private static <T extends Entity> EntityType<T> register(String name, EntityType.Builder<T> builder) {
+        ResourceKey<EntityType<?>> key = ResourceKey.create(Registries.ENTITY_TYPE, Identifier.fromNamespaceAndPath(CrankMod2.MOD_ID, name));
+        return Registry.register(BuiltInRegistries.ENTITY_TYPE, key, builder.build(key));
+    }
+
+    public static void registerModEntityTypes() {
+        CrankMod2.LOGGER.info("Registering EntityTypes for " + CrankMod2.MOD_ID);
+    }
+
+    public static void registerAttributes() {
+        FabricDefaultAttributeRegistry.register(GORILLA, GorillaEntity.createCubeAttributes());
+    }
+}
