4bca67ea583886cc8d99237a21ae9a9071a89f34
asphergers <carter44354@gmail.com>
2026-08-09 10:14:01 -0400

    fixed crash cases

--- src/main/java/net/babywipes/crankmod2/sounds/SpeakerManager.java
+++ src/main/java/net/babywipes/crankmod2/sounds/SpeakerManager.java
@@ -1,38 +1,62 @@
 package net.babywipes.crankmod2.sounds;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 import net.babywipes.crankmod2.networking.ClientboundSpeakerPayload;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class SpeakerManager {
     public record SpeakerPlayingInstance(int entityId, String playerName) {}
 
+    private static Stack<Integer> removeStack = new Stack<>();
     private static HashMap<Integer, SpeakerPlayingInstance> instanceMap = new HashMap<>();
+    private static MinecraftServer serv;
     public static void initalize() {
         ServerTickEvents.END_SERVER_TICK.register((server) -> {
-            for (var instance : instanceMap.values()) {
+            serv = server;
+            instanceMap.forEach((id, instance) -> {
                 ServerPlayer holder = server.getPlayerList().getPlayerByName(instance.playerName()); 
+                if (holder == null) {
+                    removeStack.push(id);
+                    return; 
+                }
+
+                Vec3 holderPos = holder.position();
                 int entityId = holder.asLivingEntity().getId();
                 for (var player : PlayerLookup.all(server)) {
-                    float volume = calculateSoundVolume((float)(holder.position().distanceTo(player.position())), 20f);
+                    if (player == null) { continue; }
+                    float volume = calculateSoundVolume((float)(holderPos.distanceTo(player.position())), 20f);
                     var payload = new ClientboundSpeakerPayload(new SpeakerState(false, volume, false, true, entityId));
 
                     ServerPlayNetworking.send(player, payload);
                 }
-            }
-            //server.getPlayerList().getPlayerByName(name);
+
+            });
+
+            removeStack.forEach((id) -> {
+                instanceMap.remove(id);
+                for (var player : PlayerLookup.all(server)) {
+                    if (player == null) { continue; }
+                    var payload = new ClientboundSpeakerPayload(new SpeakerState(false, 0.0f, true, false, id));
+
+                    ServerPlayNetworking.send(player, payload);
+                }
+            });
+            removeStack.clear();
         });
     }    
 
     private static float calculateSoundVolume(float distance, float maxDistance) {
         if (distance > maxDistance) { distance = maxDistance; }
-        return ((distance - 0f) / (-maxDistance)) + 1f;
+        return (float)Math.pow((double)((distance - 0f) / (-maxDistance)) + 1f, 2d);
     }
 
     public static void startPlaying(String playerName, int entityId, Level level) {
