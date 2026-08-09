e8a314c2d67cea8dcdb9a260280ee6a7a7fa3b28
jpshan <jpshan2004@outlook.com>
2026-08-08 20:21:10 -0400

    added data gen for items

--- src/main/java/net/babywipes/crankmod2/CrankMod2DataGenerator.java
+++ src/main/java/net/babywipes/crankmod2/CrankMod2DataGenerator.java
@@ -1,11 +1,14 @@
 package net.babywipes.crankmod2;
 
+import net.babywipes.crankmod2.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class CrankMod2DataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
