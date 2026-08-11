f5c5895056404de5666091a3d8e969bf353d8021
asphergers <carter44354@gmail.com>
2026-08-10 21:50:03 -0400

    seperated client and server side code

--- src/main/java/net/babywipes/crankmod2/CrankMod2Client.java
+++ src/main/java/net/babywipes/crankmod2/CrankMod2Client.java
@@ -1,13 +1,20 @@
 package net.babywipes.crankmod2;
 
+import net.babywipes.crankmod2.client.ModEntityModelLayers;
+import net.babywipes.crankmod2.entity.ModEntityTypes;
+import net.babywipes.crankmod2.entity.gorilla.GorillaEntityRenderer;
 import net.babywipes.crankmod2.networking.ModNetworking;
 import net.babywipes.crankmod2.networking.NetworkingStatics;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 
 public class CrankMod2Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         NetworkingStatics.initalize();
         ModNetworking.initalizeClient();
+		ModEntityTypes.registerModEntityTypes();
+		ModEntityModelLayers.registerModelLayers();
+		EntityRenderers.register(ModEntityTypes.GORILLA, GorillaEntityRenderer::new);
     }
 }
